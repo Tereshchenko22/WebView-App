d4ccfaee3a3959a5a5eae417806f885ecf52c9db
Vladyslav Tereshchenko <tereshchenko22dev@gmail.com>
2023-06-22 17:55:29 +0300

    update

--- app/src/main/java/com/example/testtask_2/MainActivity.kt
+++ app/src/main/java/com/example/testtask_2/MainActivity.kt
@@ -17,43 +17,58 @@ import kotlin.random.Random
 class MainActivity : AppCompatActivity() {
 
     private lateinit var webView: WebView
-    private lateinit var textNoInternet: TextView
     private lateinit var button: Button
+    private lateinit var textView: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         webView = findViewById(R.id.webView)
-        textNoInternet = findViewById(R.id.textNoInternet)
         button = findViewById(R.id.button)
+        textView = findViewById(R.id.textview)
+
+        button.setOnClickListener {
+            checkInternetConnection()
+        }
 
         checkInternetConnection()
+    }
+
+    private fun checkInternetConnection() {
+        val connectivityManager = getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        val networkInfo = connectivityManager.activeNetworkInfo
+
+        if (networkInfo != null && networkInfo.isConnected) {
+            showWebView()
+        } else {
+            showButtonScreen()
+        }
+    }
+
+    private fun showWebView() {
+        webView.visibility = View.VISIBLE
+        button.visibility = View.GONE
+        textView.visibility = View.GONE
+        webView.settings.javaScriptEnabled = true
+        webView.loadUrl("https://pin-up.ua/")
+    }
+
+    private fun showButtonScreen() {
+        webView.visibility = View.GONE
+        button.visibility = View.VISIBLE
+        textView.visibility = View.VISIBLE
+        button.setBackgroundColor(getRandomColor())
 
         button.setOnClickListener {
             checkInternetConnection()
         }
     }
 
-    private fun checkInternetConnection() {
-        val connectivityManager = getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
-        val activeNetworkInfo = connectivityManager.activeNetworkInfo
-
-        if (activeNetworkInfo != null && activeNetworkInfo.isConnected) {
-            webView.visibility = View.VISIBLE
-            textNoInternet.visibility = View.GONE
-            button.visibility = View.GONE
-            loadWebView()
-        } else {
-            webView.visibility = View.GONE
-            textNoInternet.visibility = View.VISIBLE
-            button.visibility = View.VISIBLE
-            Toast.makeText(this, "No Internet Connection", Toast.LENGTH_SHORT).show()
-        }
-    }
-
-    private fun loadWebView() {
-        webView.settings.javaScriptEnabled = true
-        webView.loadUrl("https://pin-up.ua/")
+    private fun getRandomColor(): Int {
+        val red = Random.nextInt(256)
+        val green = Random.nextInt(256)
+        val blue = Random.nextInt(256)
+        return Color.rgb(red, green, blue)
     }
 }
