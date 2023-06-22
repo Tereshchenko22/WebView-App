bc8ee149c4e2da04ab2c34556a0bbffb10d83db1
Vladyslav Tereshchenko <tereshchenko22dev@gmail.com>
2023-06-22 18:18:51 +0300

    update

--- app/src/main/java/com/example/testtask_2/MainActivity.kt
+++ app/src/main/java/com/example/testtask_2/MainActivity.kt
@@ -1,17 +1,16 @@
 package com.example.testtask_2
 
 import android.content.Context
+import android.content.res.Configuration
 import android.graphics.Color
 import android.net.ConnectivityManager
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.webkit.WebView
 import android.widget.Button
 import android.widget.TextView
-import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import kotlin.random.Random
-
 
 
 class MainActivity : AppCompatActivity() {
@@ -36,7 +35,8 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun checkInternetConnection() {
-        val connectivityManager = getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        val connectivityManager =
+            getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
         val networkInfo = connectivityManager.activeNetworkInfo
 
         if (networkInfo != null && networkInfo.isConnected) {
@@ -70,5 +70,9 @@ class MainActivity : AppCompatActivity() {
         val green = Random.nextInt(256)
         val blue = Random.nextInt(256)
         return Color.rgb(red, green, blue)
+    }
+
+    override fun onConfigurationChanged(newConfig: Configuration) {
+        super.onConfigurationChanged(newConfig)
     }
 }
